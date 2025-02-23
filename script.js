9e511ac4db21d41fa2d1787414a6be402f14bacb
elenekok <elenekokilashvili689@gmail.com>
2025-02-24 01:36:53 +0400

    feat/fix
    adding h1
    adding script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let numbers = [2, 4, 6, 8, 10];
+
+let doubledNumbers = [];
+
+numbers.forEach((numbers) => {
+  doubledNumbers.push(numbers * 2);
+});
+console.log(doubledNumbers);
